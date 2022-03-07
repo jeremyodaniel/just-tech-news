63af8c571cc999733f559ffa453afce2bdd4bd1f
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-03-06 22:34:37 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -8,16 +8,14 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new sequelize(process.env.DB_NAME, process.env.DB_USER, 
-              process.env.DB_PW, {
-              host: 'localhost',
-              dialect: mysql,
-              port: 3306
-              });
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
 }
-
 
 // This is the old connection logic prior to hosting on Heroku.
 //const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, 
